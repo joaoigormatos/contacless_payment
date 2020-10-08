ae90d5595caa739ed05f4c471c847fa8f73f87e6
Higor Moroni <moroni@higor.tech>
2020-10-07 22:49:47 -0300

    Terminando Cartão

--- app/web/pages/CreditCard/creditcard.js
+++ app/web/pages/CreditCard/creditcard.js
@@ -4,13 +4,21 @@ const formCreditCard = document.createElement('form');
 formCreditCard.id = "creditcard";
 container.appendChild(formCreditCard);
 
-function criaInput(name, type, father) {
+function criaInput(name, type, father, div, length) {
   function formatName(name) {
     const formatedName = name
       .normalize("NFD")
       .replace(/[^()a-zA-Zs]/g, "")
       .toLowerCase();
     return formatedName;
+  }
+
+  function createDiv() {
+    const div = document.createElement('div');
+    div.id = `div${name}`;
+    div.className = "small";
+    father.appendChild(div);
+    return div;
   }
 
   const label = document.createElement('label');
@@ -23,8 +31,15 @@ function criaInput(name, type, father) {
   input.type = type;
   input.name = formatedName;
 
-  father.appendChild(label);
-  father.appendChild(input);
+  if (div) {
+    const div = createDiv();
+    input.maxLength = length;
+    div.appendChild(label);
+    div.appendChild(input);
+  } else {
+    father.appendChild(label);
+    father.appendChild(input);
+  }
 }
 
 function loadCSS(url) {
@@ -35,14 +50,15 @@ function loadCSS(url) {
   document.getElementsByTagName("head").item(0).appendChild(lnk);
 }
 
-criaInput("Numero do Cartão", "text", formCreditCard);
-criaInput("Nome do titular", "text", formCreditCard);
+criaInput("Numero do Cartão", "text", formCreditCard, false);
+criaInput("Nome do titular", "text", formCreditCard, false);
 
 const divCvcValidade = document.createElement('div');
 divCvcValidade.id = "divCvcValidade";
 formCreditCard.appendChild(divCvcValidade);
 
-criaInput("CVC", "text", divCvcValidade);
-criaInput("Validade", "text", divCvcValidade);
+criaInput("CVC", "text", divCvcValidade, true, 3);
+criaInput("Validade", "text", divCvcValidade, true, 5);
+
 loadCSS("../Login/login.css");
 loadCSS("creditcard.css");
