4236451f4f1a8870767d2677afc40dd78cda48bd
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-10-10 12:38:17 -0300

    Refactored code to class based component

--- app/web/pages/AddCurrency/renderAddCurrencyComponent.js
+++ app/web/pages/AddCurrency/renderAddCurrencyComponent.js
@@ -1,136 +1,76 @@
-export const renderMobile = () => {
-  const body = document.body;
-  const main = document.createElement('main');
-  body.appendChild(main);
+import CustomComponent from '../../assets/js/CustomComponent.js';
 
-  renderContent(main);
+class AddCurrencyComponent extends CustomComponent {
+  renderSuccessScreen(main) {
+    const success = document.createElement('div');
+    success.id = 'success';
+    success.classList.add('hidden');
+    success.style.display = 'none';
+    main.appendChild(success);
 
-  const head = document.head;
-  const style = document.createElement('link');
-  style.id = 'mobile';
-  style.rel = 'stylesheet';
-  style.href = './mobile.css';
-  head.appendChild(style);
+    const image = document.createElement('img');
+    image.src = '../../assets/images/success-icon.svg';
+    success.appendChild(image);
 
-  return document;
-};
+    const title = document.createElement('h2');
+    title.id = 'success-header';
+    success.appendChild(title);
 
-export const renderSuccessScreen = (main) => {
-  const success = document.createElement('div');
-  success.id = 'success';
-  success.classList.add('hidden');
-  success.style.display = 'none';
-  main.appendChild(success);
+    const message = document.createElement('p');
+    message.id = 'success-message';
+    success.appendChild(message);
 
-  const image = document.createElement('img');
-  image.src = '../../assets/images/success-icon.svg';
-  success.appendChild(image);
+    return main;
+  }
+  renderContent(main) {
+    const container = document.createElement('div');
+    container.id = 'container';
+    container.classList.add('wrapper');
+    main.appendChild(container);
 
-  const title = document.createElement('h2');
-  title.id = 'success-header';
-  success.appendChild(title);
+    const title = document.createElement('h2');
+    title.innerHTML = 'Adicionar saldo';
+    container.appendChild(title);
+    const subtitle = document.createElement('h3');
+    subtitle.innerHTML = 'Informe o valor';
+    container.appendChild(subtitle);
 
-  const message = document.createElement('p');
-  message.id = 'success-message';
-  success.appendChild(message);
+    const input = document.createElement('input');
+    input.dir = 'rtl';
+    input.type = 'currency';
+    input.value = '';
+    input.placeholder = 'R$ 0,00';
+    container.appendChild(input);
 
-  return main;
-};
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('btn-container');
+    container.appendChild(buttonContainer);
 
-export const renderModal = (body) => {
-  const modalOpener = document.createElement('button');
-  modalOpener.id = 'myBtn';
-  modalOpener.innerHTML = 'OPEN MODAL';
-  body.appendChild(modalOpener);
+    const creditButton = document.createElement('button');
+    creditButton.innerHTML = 'Cartão de crédito';
+    creditButton.id = 'credit';
+    creditButton.classList.add('btn');
+    creditButton.classList.add('btn-yellow');
+    buttonContainer.appendChild(creditButton);
 
-  const modalBackgound = document.createElement('div');
-  modalBackgound.id = 'myModal';
-  modalBackgound.classList.add('modal');
-  body.appendChild(modalBackgound);
+    const boletoButton = document.createElement('button');
+    boletoButton.innerHTML = 'Boleto';
+    boletoButton.id = 'boleto';
+    boletoButton.classList.add('btn');
+    boletoButton.classList.add('btn-yellow');
+    buttonContainer.appendChild(boletoButton);
 
-  const modalContent = document.createElement('div');
-  modalContent.classList.add('modal-content');
-  modalBackgound.appendChild(modalContent);
+    this.renderSuccessScreen(main);
 
-  const modalCloseButton = document.createElement('span');
-  modalCloseButton.classList.add('close');
-  modalCloseButton.innerHTML = '&times';
-  modalContent.appendChild(modalCloseButton);
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = '../../assets/js/currencyTextInputHelper.js';
+    main.appendChild(script);
 
-  return modalContent;
-};
-
-export const renderDesktop = () => {
-  const body = document.body;
-
-  const modalContent = renderModal(body);
-
-  renderContent(modalContent);
-
-  const modalScript = document.createElement('script');
-  modalScript.type = 'module';
-  modalScript.src = '../../assets/js/modalHandler.js';
-  body.appendChild(modalScript);
-
-  const style = document.createElement('link');
-  style.id = 'desktop';
-  style.rel = 'stylesheet';
-  style.href = './desktop.css';
-  document.head.appendChild(style);
-
-  return document;
-};
-
-export const renderContent = (main) => {
-  const container = document.createElement('div');
-  container.id = 'container';
-  container.classList.add('wrapper');
-  main.appendChild(container);
-
-  const title = document.createElement('h2');
-  title.innerHTML = 'Adicionar saldo';
-  container.appendChild(title);
-  const subtitle = document.createElement('h3');
-  subtitle.innerHTML = 'Informe o valor';
-  container.appendChild(subtitle);
-
-  const input = document.createElement('input');
-  input.dir = 'rtl';
-  input.type = 'currency';
-  input.value = '';
-  input.placeholder = 'R$ 0,00';
-  container.appendChild(input);
-
-  const buttonContainer = document.createElement('div');
-  buttonContainer.classList.add('btn-container');
-  container.appendChild(buttonContainer);
-
-  const creditButton = document.createElement('button');
-  creditButton.innerHTML = 'Cartão de crédito';
-  creditButton.id = 'credit';
-  creditButton.classList.add('btn');
-  creditButton.classList.add('btn-yellow');
-  buttonContainer.appendChild(creditButton);
-
-  const boletoButton = document.createElement('button');
-  boletoButton.innerHTML = 'Boleto';
-  boletoButton.id = 'boleto';
-  boletoButton.classList.add('btn');
-  boletoButton.classList.add('btn-yellow');
-  buttonContainer.appendChild(boletoButton);
-
-  renderSuccessScreen(main);
-
-  const script = document.createElement('script');
-  script.type = 'module';
-  script.src = '../../assets/js/currencyTextInputHelper.js';
-  main.appendChild(script);
-
-  return main;
-};
-
-if (window.innerWidth <= 400) {
-  renderMobile();
-} else {
-  renderDesktop();
+    return main;
+  }
 }
+
+export default AddCurrencyComponent;
+
+new AddCurrencyComponent().renderComponent();
