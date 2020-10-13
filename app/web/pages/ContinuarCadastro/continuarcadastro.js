6500c90e7d7c492f998e85821605159584747289
Higor Moroni <moroni@higor.tech>
2020-10-13 15:46:57 -0300

    HandleInput

--- app/web/pages/ContinuarCadastro/continuarcadastro.js
+++ app/web/pages/ContinuarCadastro/continuarcadastro.js
@@ -3,7 +3,10 @@ const inputs = document.querySelectorAll('input[type=file]');
 const input = { front: inputs[0], back: inputs[1], selfie: inputs[2] }
 
 const handleChangeFileInput = e => {
+  alert('entrou');
   console.log(e);
 }
 
-input.front.addEventListener('change' (e) => handleChangeFileInput(e));
+console.log(input.front)
+
+input.front.addEventListener('change', (e) => handleChangeFileInput(e));
